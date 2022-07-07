b30e77d617b7a4d436383ae9046d8381c6131110
Diego Duarte Fernández <diegotech1499@gmail.com>
2022-07-07 10:54:05 -0600

    bubble sort

---
+++ Bubble.java
@@ -0,0 +1,35 @@
+public class Bubble{
+
+    public int[] bubbleSort(int[] array){
+        
+        int arrayLength = array.length;
+        
+        for(int i = 0; i < arrayLength; i++){
+            for(int j = 0; j < arrayLength-i-1; j++){
+
+                if(array[j] > array[j+1]){
+                    int temporalNumber = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temporalNumber; 
+                    
+                }
+            }
+        }
+
+        return array;
+    }
+
+    public String toString(int[] array){
+        String output = "";
+        for(int i = 0; i < array.length; i++){
+            output += "["+ array[i] +"] ";
+        }
+        return output;
+    }
+
+    public static void main(String args[]){
+        Bubble bubble = new Bubble();
+        int[] array = {-23, 1, -8, 2, 4, 3, 6, -90, 37, 299}; 
+        System.out.println(bubble.toString(bubble.bubbleSort(array)));
+    }
+}
